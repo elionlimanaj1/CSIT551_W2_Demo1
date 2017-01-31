e7db6024964277bddbc2e86f04a40ac35f683ec9
Elion Limanaj <elionlimanaj@Elions-MBP.aesny.org>
2017-01-31 09:54:41 -0500

    Initial commit

---
+++ app/src/main/java/com/elionlimanaj/csit551_w2_demo1/utils/Utils.java
@@ -0,0 +1,9 @@
+package com.elionlimanaj.csit551_w2_demo1.utils;
+
+/**
+ * Created by elionlimanaj on 1/30/17.
+ */
+
+public class Utils {
+    public final static String APP_TAG = "ACTIVITY 1 TAG";
+}
